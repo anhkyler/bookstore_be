5c93126deb60b263bbdc76c55bcfa17f3c7044c1
Hieu So <hieuphso@gmail.com>
2023-07-11 21:45:43 -0400

    expose ids for dynamic menu

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -2,15 +2,31 @@ package com.luv2code.ecommerce.config;
 
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+	private EntityManager entityManager;
+	
+	@Autowired
+	public MyDataRestConfig(EntityManager theentiEntityManager) {
+		this.entityManager = theentiEntityManager;
+	}
+	
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
@@ -27,6 +43,17 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
+        exposeIds(config);
+    }
+    
+    private void exposeIds(RepositoryRestConfiguration config) {
+    	Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+    	List<Class> entityClasses = new ArrayList<>();
+    	for(EntityType temEntityType : entities) {
+    		entityClasses.add(temEntityType.getJavaType());
+    	}
+    	
+    	Class[] domainTypes = entityClasses.toArray(new Class[0]);
+    	config.exposeIdsFor(domainTypes);
     }
 }
